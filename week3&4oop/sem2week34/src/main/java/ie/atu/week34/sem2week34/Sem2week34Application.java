2e0db3d09b9682cfd430e29352865e5af7049496
DanielMaku-STUDENT <g00467390@atu.ie>
2026-02-18 15:04:55 +0000

    start

---
+++ week3&4oop/sem2week34/src/main/java/ie/atu/week34/sem2week34/Sem2week34Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week34.sem2week34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2week34Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem2week34Application.class, args);
+    }
+
+}
---
+++ week3&4oop/sem2week34/src/test/java/ie/atu/week34/sem2week34/Sem2week34ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week34.sem2week34;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2week34ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
