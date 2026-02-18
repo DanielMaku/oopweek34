786a5e9dc151ed48b6410c5d28a6628ac4b1c872
DanielMaku-STUDENT <g00467390@atu.ie>
2026-02-18 15:41:24 +0000

    added a few(taking break)

---
+++ week3&4oop/sem2week34/src/main/java/ie/atu/week34/sem2week34/controller/CalculatorController.java
@@ -0,0 +1,23 @@
+package ie.atu.week34.sem2week34.controller;
+
+import ie.atu.week34.sem2week34.service.CalculatorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calc")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/add")
+    public String add (String a, String b) {
+        return "";
+    }
+
+
+}
